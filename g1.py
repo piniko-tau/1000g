9887d11c6219a7ded0eac62ab7c101ffec96b675
user1 <user@mail.com>
2016-05-10 07:59:35 +0300

    added ilter mind data by drugs

--- g1.py
+++ g1.py
@@ -147,7 +147,7 @@ parser.add_argument("-load_DBIdb_tables_preprocess",help='list how to load_DBIdb
 
 parser.add_argument("-create_drugs_genes_table",help='create_drugs_genes_table',action="store_true")
 
-parser.add_argument("filter_mind_table_by_drugs",help='filter_mind_table_by_drugs',metavar='filter_mind_table_by_drugs')
+parser.add_argument("-filter_mind_table_by_drugs",help='filter_mind_table_by_drugs',metavar='filter_mind_table_by_drugs')
 
 parser.add_argument("-mind_export_ml",help='export a ml dataset of mind data',metavar='mind_export_ml')
 
@@ -1726,17 +1726,19 @@ try:
         table_mind_data_n_rs_ensorted_by_gene_posann_by_drug = args.filter_mind_table_by_drugs+"_by_drug"
         table_gene_name_and_drug_name_2_intermediate = "table_gene_name_and_drug_name_2_intermediate"
 
-        check_overwrite_table(table_gene_name_and_drug_name_2_intermediate)
+        if not (check_table_exists(table_gene_name_and_drug_name_2_intermediate)):
+            
+            check_overwrite_table(table_gene_name_and_drug_name_2_intermediate)
 
-        #copy and alter drugs table gene_name column to gene_name2 for easy joining
-        print(cur.mogrify("CREATE TABLE %s AS SELECT * FROM gene_name_and_drug_name",(AsIs(table_gene_name_and_drug_name_2_intermediate),)))
-        cur.execute("CREATE TABLE %s AS SELECT * FROM gene_name_and_drug_name",(AsIs(table_gene_name_and_drug_name_2_intermediate),))
-        conn.commit()
+            #copy and alter drugs table gene_name column to gene_name2 for easy joining
+            print(cur.mogrify("CREATE TABLE %s AS SELECT * FROM gene_name_and_drug_name",(AsIs(table_gene_name_and_drug_name_2_intermediate),)))
+            cur.execute("CREATE TABLE %s AS SELECT * FROM gene_name_and_drug_name",(AsIs(table_gene_name_and_drug_name_2_intermediate),))
+            conn.commit()
 
-        #copy and alter drugs table gene_name column to gene_name2 for easy joining
-        print(cur.mogrify("alter table  %s rename column gene_name to gene_name2",(AsIs(table_gene_name_and_drug_name_2_intermediate),)))
-        cur.execute("alter table  %s rename column gene_name to gene_name2",(AsIs(table_gene_name_and_drug_name_2_intermediate),))
-        conn.commit()
+            #copy and alter drugs table gene_name column to gene_name2 for easy joining
+            print(cur.mogrify("alter table  %s rename column gene_name to gene_name2",(AsIs(table_gene_name_and_drug_name_2_intermediate),)))
+            cur.execute("alter table  %s rename column gene_name to gene_name2",(AsIs(table_gene_name_and_drug_name_2_intermediate),))
+            conn.commit()
 
         check_overwrite_table(table_mind_data_n_rs_ensorted_by_gene_posann_by_drug)
 
