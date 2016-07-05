99bd0baee0ffc5208d807d57431e9bfe9f593e47
user1 <user@mail.com>
2016-07-05 08:23:31 +0300

    start  alt export

--- g1.py
+++ g1.py
@@ -1532,7 +1532,7 @@ try:
             export_file.write("'patient','diagnosis',")
 
 #add alt gene here
-            cur.execute("select gene_name||' | '||rsids||' | '||gene_drugs from %s group by gene_name order by gene_name;",(AsIs(table_mind_export_ml_with_drugs_alt_header_rsids_and_drugs),))
+            cur.execute("select 'gene_name: '||gene_name||' | rsids: '||rsids||' |  '||gene_drugs||' | '||interacting_genes                     from %s order by gene_name;",(AsIs(table_mind_export_ml_with_drugs_alt_header_rsids_and_drugs),))
 
 
             for i2 in cur.fetchall():
