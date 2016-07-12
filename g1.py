ce11bce7b29f44890c7626bb1a6bc94f7a179cbe
user1 <user@mail.com>
2016-07-12 10:31:38 +0300

    org export drugs

--- g1.py
+++ g1.py
@@ -1385,8 +1385,8 @@ try:
 
 
 
-        rstable = args.mind_export_ml_with_drugs.replace("_ensorted_by_gene_posann_by_drug_altyesyesyes"
-                                                         "","")
+        rstable = args.mind_export_ml_with_drugs.replace("_ensorted_by_gene_posann_by_drug_alt","")
+        rstable = args.mind_export_ml_with_drugs.replace("_ensorted_by_gene_posann_by_drug","")
 
 
         force_check_overwrite_table(table_mind_export_ml_with_drugs_header_rsids)
@@ -1470,6 +1470,120 @@ try:
     def time_it():
         print "time: " + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
 
+
+##new export_u
+    def mind_export_ml_with_drugs_u():
+
+        # get diagnosis, patient name,peptides string,gene in single line and string_agg it , output to file , for each of the four tables, for each patient
+
+
+        # pydb=> select gene_name , string_agg(rsid,' ' order by rsid) as rsids ,string_agg(drugs_info,' ' order by drugs_info) as gene_drugs from (select distinct gene_name,rsid,drugs_info from mind_data_1_rs_ensorted_by_gene_posann_by_drug) as t1 group by t1.gene_name order by t1.gene_name;
+
+        # pydb=> select gene_name ,string_agg(drugs_info,' ' order by drugs_info) as gene_drugs from (select distinct gene_name,drugs_info from mind_data_1_rs_ensorted_by_gene_posann_by_drug) as t1 group by t1.gene_name order by t1.gene_name;
+
+
+        table_mind_export_ml_with_drugs_header_rsids = args.mind_export_ml_with_drugs + "_header_rsids"
+
+        table_mind_export_ml_with_drugs_header_drugs = args.mind_export_ml_with_drugs + "_header_drugs"
+
+        table_mind_export_ml_with_drugs_header_rsids_and_drugs = args.mind_export_ml_with_drugs + "_h_rsids_drugs"
+
+        rstable = args.mind_export_ml_with_drugs.replace("_ensorted_by_gene_posann_by_drug_alt", "")
+
+        check_overwrite_table(table_mind_export_ml_with_drugs_header_rsids)
+
+        # create dist header for ml with rsids here .....
+        print(cur.mogrify(
+            "CREATE TABLE %s AS select gene_name , string_agg(rsid,' ' order by rsid) as rsids from (select distinct gene_name,rsid from %s) as t1 group by t1.gene_name order by t1.gene_name; ",
+            (AsIs(table_mind_export_ml_with_drugs_header_rsids), AsIs(args.mind_export_ml_with_drugs),)))
+        cur.execute(
+            "CREATE TABLE %s AS select gene_name , string_agg(rsid,' ' order by rsid) as rsids from (select distinct gene_name,rsid from %s) as t1 group by t1.gene_name order by t1.gene_name; ",
+            (AsIs(table_mind_export_ml_with_drugs_header_rsids), AsIs(args.mind_export_ml_with_drugs),))
+        conn.commit()
+
+        check_overwrite_table(table_mind_export_ml_with_drugs_header_drugs)
+
+        # create dist header with drugs for ml here .....
+        print(cur.mogrify(
+            "CREATE TABLE %s AS select gene_name as gene_name2 ,string_agg(drugs_info,' ' order by drugs_info) as gene_drugs from (select distinct gene_name,drugs_info from %s) as t1 group by t1.gene_name order by t1.gene_name; ",
+            (AsIs(table_mind_export_ml_with_drugs_header_drugs), AsIs(args.mind_export_ml_with_drugs),)))
+        cur.execute(
+            "CREATE TABLE %s AS select gene_name as gene_name2,string_agg(drugs_info,' ' order by drugs_info) as gene_drugs from (select distinct gene_name,drugs_info from %s) as t1 group by t1.gene_name order by t1.gene_name; ",
+            (AsIs(table_mind_export_ml_with_drugs_header_drugs), AsIs(args.mind_export_ml_with_drugs),))
+        conn.commit()
+
+        check_overwrite_table(table_mind_export_ml_with_drugs_header_rsids_and_drugs)
+
+        # join the previouse tables into one final header table
+        print(
+        cur.mogrify("CREATE TABLE %s AS SELECT * FROM %s inner join %s on (%s.gene_name2 = %s.gene_name)", (
+        AsIs(table_mind_export_ml_with_drugs_header_rsids_and_drugs),
+        AsIs(table_mind_export_ml_with_drugs_header_rsids), AsIs(table_mind_export_ml_with_drugs_header_drugs),
+        AsIs(table_mind_export_ml_with_drugs_header_drugs),
+        AsIs(table_mind_export_ml_with_drugs_header_rsids),)))
+        cur.execute("CREATE TABLE %s AS SELECT * FROM %s inner join %s on (%s.gene_name2 = %s.gene_name)", (
+        AsIs(table_mind_export_ml_with_drugs_header_rsids_and_drugs),
+        AsIs(table_mind_export_ml_with_drugs_header_rsids), AsIs(table_mind_export_ml_with_drugs_header_drugs),
+        AsIs(table_mind_export_ml_with_drugs_header_drugs),
+        AsIs(table_mind_export_ml_with_drugs_header_rsids),))
+        conn.commit()
+
+        widgets = ['processing query -> ' + table1000g + ' :', Percentage(), ' ', Bar(marker=RotatingMarker()),
+                   ' ', ETA(), ' ', FileTransferSpeed()]
+
+        pbar = ProgressBar(widgets=widgets, maxval=10000000).start()
+
+        with open('exported_mind_by_drugs_alt_u.csv', "a") as export_file:
+
+            # all 4 files are appended
+            # I delete the header line manually for the 3 last files
+            # this is useful for quality control
+
+
+            # add |drugs_info here after "||rsids"
+            export_file.write("'patient','diagnosis',")
+
+            cur.execute("select gene_name||' | '||rsids||' | '||gene_drugs from %s order by gene_name;;",
+                        (AsIs(table_mind_export_ml_with_drugs_header_rsids_and_drugs),))
+
+            for i2 in cur.fetchall():
+                # output this line with only commas between columns !
+
+                i2_filter1 = re.sub('(\()|(\[)|(\])|(\))', '', str(i2))
+
+                i2_filter2 = re.sub(',', '', i2_filter1)
+
+                export_file.write(i2_filter2)
+
+                export_file.write(',')
+
+            export_file.write("\n")
+
+            cur.execute(
+                "select column_name from information_schema.columns where table_name = \'%s\' and ( column_name ~ \'^sz.*[1-9]\' or column_name ~ \'^cg.*[1-9]\' or column_name ~ \'^el.*[1-9]\' or column_name ~ \'^gc.*[1-9]\' );",
+                (AsIs(args.mind_export_ml_with_drugs),))
+
+            for hg2 in pbar(query2list()):
+
+                export_file.write('\'' + hg2 + '\'')
+                export_file.write(',')
+
+                # add correct diagnosis for each patient , idnum=1 is the row with diagnosis for each patient column
+                cur.execute("select %s from %s where idnum='1' limit 1;", (AsIs(hg2), AsIs(rstable),))
+
+                for i2 in cur.fetchall():
+                    export_file.write(re.sub('(\()|(\[)|(\])|(\))', '', str(i2)))
+
+                cur.execute(
+                    "select peptid_group from (select gene_name , string_agg(rsid,',' order by rsid) as rsids, string_agg(peptid_group,'') as peptid_group from (select distinct gene_name,rsid,mytable.t1||mytable.t2||mytable.t3  as peptid_group from (select gene_name,rsid,%s, case when substr(%s,1,1) = allele1 then '+'||peptide1 when substr(%s,1,1) = allele2 then '+'||peptide2 when substr(%s,1,1) = allele3 then '+'||peptide3 when substr(%s,1,1) = opallele1 then '-'||peptide1 when substr(%s,1,1) = opallele2 then '-'||peptide2 when substr(%s,1,1) = opallele3 then '-'||peptide3 end as t1 , case when substr(%s,3,1) = allele1 then '+'||peptide1 when substr(%s,3,1) = allele2 then '+'||peptide2 when substr(%s,3,1) = allele3 then '+'||peptide3 when substr(%s,3,1) = opallele1 then '-'||peptide1 when substr(%s,3,1) = opallele2 then '-'||peptide2 when substr(%s,3,1) = opallele3 then '-'||peptide3 end as t2 ,case when substr(%s,5,1) = '' then '' when substr(%s,5,1) = allele1 then '+'||peptide1 when substr(%s,5,1) = allele2 then '+'||peptide2 when substr(%s,5,1) = allele3 then '+'||peptide3 when substr(%s,5,1) = opallele1 then '-'||peptide1 when substr(%s,5,1) = opallele2 then '-'||peptide2 when substr(%s,5,1) = opallele3 then '-'||peptide3 end as t3 from  %s) as mytable )as t1 group by t1.gene_name order by t1.gene_name) as t5;",
+                    (AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2),
+                     AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2),
+                     AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(hg2), AsIs(args.mind_export_ml_with_drugs),))
+
+                for i2 in cur.fetchall():
+                    export_file.write(re.sub('(\()|(\[)|(\])|(\))', '', str(i2)))
+
+                export_file.write("\n")
 
 
     def mind_export_ml_with_drugs_alt():
