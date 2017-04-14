a4f0185b981c7d3002e01ac3128aabab17f96a8c
piniko <piniko@post.tau.ac.il>
2017-04-15 02:02:54 +0300

    @$

--- nih3_datamine.py
+++ nih3_datamine.py
@@ -25,8 +25,8 @@ parser = argparse.ArgumentParser(prog='nih3_datamine',usage='python nih3_datamin
 [-args.analyse_col_file]\
 ',\
 description='Load annotated snp database & Create a 1000G sql table from all Chromosomes - using a connection to a postgresql DB.')
-parser.add_argument("-analyse_col_file",required=True,help='analyse_col_file print col var table',metavar='analyse_col_file')
-parser.add_argument("-nih3_file",required=True,help='nih3_file file to process',metavar='nih3_file')
+parser.add_argument("-analyse_col_file",help='analyse_col_file print col var table',metavar='analyse_col_file')
+parser.add_argument("-nih3_file",help='nih3_file file to process',metavar='nih3_file')
 parser.add_argument("-v", "--verbose", help="increase output verbosity",action="store_true")
 args = parser.parse_args()
 
@@ -44,10 +44,4 @@ if args.analyse_col_file:
 
 if args.nih3_file:
 
-# cur.execute("INSERT INTO %s SELECT * FROM ( SELECT * , SUBSTR(peptides,1 , 1) AS peptide1 , SUBSTR(peptides,3 , 1) AS peptide2 , SUBSTR(peptides,5 , 1) AS peptide3 FROM %s ) AS ptt WHERE (peptide1!=peptide2) OR (peptide1!=peptide3) OR (peptide2!=peptide3)",(AsIs(snptablenasma),AsIs(snptablena),))
-#
-#  cur.execute("CREATE TABLE %s AS SELECT * FROM %s ORDER BY codons",(AsIs(snptable_s_c),AsIs(snptablenasma),))
-#
-#  cur.execute("CREATE TABLE %s AS SELECT * FROM %s ORDER BY \"chromStart\"",(AsIs(snptable_s_ch_filtered_final),AsIs(snptable_s_c),))
-#  conn.commit()
-
+    '''   -retain only amino change, order by codons by abc, order by chrom pos asc'''
