ec1cc4efeb55a60f4a7327b4d5170992ecf7b04f
piniko <piniko@post.tau.ac.il>
2017-06-04 23:53:42 +0300

    @$

--- nih3_datamine.py
+++ nih3_datamine.py
@@ -30,10 +30,19 @@ parser.add_argument("-analyse_col_file",help='analyse_col_file print col var tab
 parser.add_argument("-nih3_file",help='nih3_file file to process',metavar='nih3_file')
 parser.add_argument("-rsid_2_gene_table",help='get rsid to gene table from ensemble',action="store_true")
 parser.add_argument("-rsid_2_gene_table_test",help='get rsid to gene table from ensemble',action="store_true")
+parser.add_argument("-get_peptides_table",help='get peptide table from ucsc',action="store_true")
 parser.add_argument("-nih3_merge_genemnames",help='inner join nih3 and variation_genenames',action="store_true")
+parser.add_argument("-nih3_merge_peptides",help='inner join nih3 and ucsc peptides',action="store_true")
+parser.add_argument("-nih3_make_ml_table",help='nih3_make_ml_table',action="store_true")
 parser.add_argument("-to_csv",help='write merge to file',action="store_true")
 parser.add_argument("-v", "--verbose", help="increase output verbosity",action="store_true")
 args = parser.parse_args()
+
+#functions:
+def get_df_memory_usage(df1):
+    print '\nmem usage:\n'
+    print (df1.memory_usage(index=True).sum())
+    print '\n\n'
 
 #setup logging option
 if args.verbose:
@@ -45,6 +54,8 @@ if args.analyse_col_file:
 
     for col in pd1:
         print (pd1[col].value_counts())
+
+
 if args.nih3_file:
     pd1=pd.read_csv(args.nih3_file, sep='\t')
     drop_row_list=[]
@@ -55,8 +66,6 @@ if args.nih3_file:
             drop_row_list.append(pd1.index[nrow])
             print(pd1.index[nrow])
     pd2=pd1.drop(pd1.index[drop_row_list])
-    print '\nnih3_file :\n'
-    print(pd2.to_string())
 
 if args.rsid_2_gene_table:
     if not os.path.exists("./variation.txt.gz"):
@@ -97,6 +106,16 @@ if args.rsid_2_gene_table_test:
     print(pd34.to_string())
 
 
+if args.get_peptides_table:
+    if not os.path.exists("./snp141CodingDbSnp.txt.gz"):
+        print "getting snp141CodingDbSnp.txt.gz from ucsc..."
+        urllib.urlretrieve('http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/snp141CodingDbSnp.txt.gz', "snp141CodingDbSnp.txt.gz")
+        print "done."
+        print "loading snp141CodingDbSnp into DF"
+        #here :
+        pd3=pd.read_csv('./snp141CodingDbSnp.txt.gz',names=['smallint','chrom','chromStart','chromEnd','name','transcript','frame','alleleCount',
+    'funcCodes','alleles','codons','peptides','peptide1','peptide2','peptide3'],compression='gzip', sep='\t')
+        print "done"
 
 
 if args.nih3_merge_genemnames:
@@ -104,6 +123,20 @@ if args.nih3_merge_genemnames:
     pd234=pd.merge(pd2, pd34, left_on='ID',right_on='rs_name', how='inner')
     print '\nnih3_merge_genemnames :\n'
     print(pd234.to_string())
+
+if args.nih3_make_ml_table:
+    #SZPABR0002
+    print'ml table\n\n'
+    print(pd234['ID'])
+    print(pd234['gene_name'])
+    print(pd234.groupby('gene_name')['ID'].apply(list))
+    pd6=pd234.groupby('gene_name')['ID'].apply(list)
+    pd7=pd234.add(pd6)
+    print(pd7.to_string())
+
+if args.nih3_merge_peptides:
+    pass
+
 
 if args.to_csv:
     pd234.to_csv('out.csv',index=False)
